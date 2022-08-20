53faff152257cadc7679d9633e41b180ccf6631d
jaykishanstax <jaykishanss23@gmail.com>
2022-08-20 17:30:50 +0200

    Updated health check api

--- admin/src/main/java/com/java/store/admin/controller/AdminController.java
+++ admin/src/main/java/com/java/store/admin/controller/AdminController.java
@@ -1,15 +1,18 @@
 package com.java.store.admin.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("admin")
+@RequestMapping()
 public class AdminController {
 
-    @GetMapping("/data")
-    public String returnData(){
-        return "Test";
+    @GetMapping("/health")
+    public ResponseEntity<?> health(){
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
